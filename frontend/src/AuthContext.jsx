c08c669e67e03a5cf3d2aed8a6000cd866e89749
AnsariAhmedFrontEnd <am27043@gmail.com>
2024-11-11 08:32:14 -0300

    Setting up the routed

---
+++ frontend/src/AuthContext.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      setUser(userId);
+    }
+  }, []);
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
